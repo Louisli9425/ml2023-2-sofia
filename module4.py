dc9a5faa7312b8ec74a7eb2ff452120c0bc2f614
Louisli9425 <149336606+Louisli9425@users.noreply.github.com>
2023-10-29 21:15:51 -0700

    Update module4.py

--- module4.py
+++ module4.py
@@ -1,1 +1,15 @@
+N = int(input("Please enter a positive integer number here: "))
 
+numbers = []
+
+for i in range(N):
+    num = int(input("You will need to provide total  " + str(N) + f" numbers. Please Enter number {i + 1}: "))
+    numbers.append(num)
+
+X = int(input("Please enter test integer X: "))
+
+if X in numbers:
+    index = numbers.index(X) + 1
+    print(f"The sequence index number of test integer X is {index}.")
+else:
+    print("-1")
